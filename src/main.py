1164782ba2bc75da842ed0523439a9e49842d597
Murph <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-07-10 16:40:52 +0200

    performance

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from data_loading import loadData
+from architecture import defineModel
+from test import testModel
+from train import trainModel
+
+X_train, y_train, X_test = loadData()
+model = defineModel()
+
+trainModel(
+    model, X_train, y_train,
+    epochs=1,
+    batch_size=64,
+    validation_split=0.2,
+    save=(True, "model1")
+)
+
+testModel(model, X_test)
--- src/test.py
+++ src/test.py
@@ -1,12 +1,14 @@
-# evaluation metric functions
 import matplotlib.pyplot as plt
-from data_loading import X_test
-from architecture import model
-import train
 
-print("\n> Testing the model...")
-pred = model.predict(X_test)
 
-plt.imshow(X_test[30].reshape(96, 96), cmap='gray')
-plt.scatter(pred[30][0::2], pred[33][1::2], c='b', marker='.')
-plt.show()
+def testModel(model, X_test):
+    """ 
+    Tests the model on the test data and shows a result.
+    """
+
+    print("\n> Testing the model...")
+    pred = model.predict(X_test)
+
+    plt.imshow(X_test[30].reshape(96, 96), cmap='gray')
+    plt.scatter(pred[30][0::2], pred[33][1::2], c='b', marker='.')
+    plt.show()
