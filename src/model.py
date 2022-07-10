15e06a83560cb836f3c9d8333a8ae5473e50e4a8
Murph <62220780+StrangeGirlMurph@users.noreply.github.com>
2022-07-10 14:32:12 +0200

    foundation

--- src/model.py
+++ src/model.py
@@ -1,2 +1,1 @@
 # trained model file
-import tensorflow as tf
--- src/test.py
+++ src/test.py
@@ -1,2 +1,12 @@
 # evaluation metric functions
-import tensorflow as tf
+import matplotlib.pyplot as plt
+from data_loading import X_test
+from architecture import model
+import train
+
+print("\n> Testing the model...")
+pred = model.predict(X_test)
+
+plt.imshow(X_test[30].reshape(96, 96), cmap='gray')
+plt.scatter(pred[30][0::2], pred[33][1::2], c='b', marker='.')
+plt.show()
