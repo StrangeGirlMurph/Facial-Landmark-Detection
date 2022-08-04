fixed masking mistake

src/training.py
@@ -26,15 +26,22 @@ def trainModel(model, X_train, y_train, epochs=50, batch_size=256, validation_sp
         showTrainingHistory(history)
 
 
+def filter_mask(y_true, y_pred):
+    mask = tf.math.logical_not(tf.math.equal(y_true, tf.constant(-1.)))
+    y_true = tf.boolean_mask(y_true, mask)
+    y_pred = tf.boolean_mask(y_pred, mask)
+    return y_true, y_pred
+
+
 def masked_mean_squared_error(y_true, y_pred):
+    y_true, y_pred = filter_mask(y_true, y_pred)
     loss = tf.square(y_true - y_pred)  # Mean Squared Error
-    loss = tf.where(y_true != -1., loss, 0.)
     return tf.reduce_mean(loss)
 
 
 def masked_mean_absolute_error(y_true, y_pred):
+    y_true, y_pred = filter_mask(y_true, y_pred)
     loss = tf.abs(y_true - y_pred)  # Mean Absolute Error
-    loss = tf.where(y_true != -1., loss, 0.)
     return tf.reduce_mean(loss)
 
 # Doesnt work yet. Only outputs 0.0
@@ -48,9 +55,9 @@ def masked_mean_absolute_error(y_true, y_pred):
 
 
 def masked_accuracy(y_true, y_pred):
+    y_true, y_pred = filter_mask(y_true, y_pred)
     diff = tf.abs(y_true - y_pred)
-    diff = tf.where(y_true != -1., diff, 0.)
-    passed = tf.math.count_nonzero(diff < 1)  # in margin of one pixel the prediction is counted as correct
+    passed = tf.math.count_nonzero(diff < 2)  # in margin of one pixel the prediction is counted as correct
     masked_accuracy.inMargin += passed.numpy()
     masked_accuracy.total += tf.size(diff).numpy()
     return masked_accuracy.inMargin / masked_accuracy.total